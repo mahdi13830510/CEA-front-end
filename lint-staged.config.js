1fd382354c3714051dd354c441c94d0207f7c71b
mahdi13830510 <mahdi.dlt13830510@Gmail.com>
2025-06-04 02:04:33 +0330

    chore: add `lint-staged` config file

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    '*.{js,ts,tsx,jsx}': ['eslint --fix', 'prettier --write'],
+    '*.json': ['prettier --write'],
+    '*.css': ['prettier --write'],
+    '*.md': ['prettier --write']
+  };
+  
